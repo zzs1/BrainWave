fc4943561cf091625b5e8ecf6bc02bddc97ebb51
JerryWuOn <Jon4@my.bcit.ca>
2023-10-03 10:45:41 -0700

    add toggle theme

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import { Text, StyleSheet, View, useColorScheme } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, useColorScheme, Switch } from 'react-native';
+import { StatusBar } from 'expo-status-bar'; // automatically switches bar style based on theme!
 
 export default function App() {
   const colorScheme = useColorScheme();
+  const [isDarkMode, setIsDarkMode] = useState(colorScheme === 'dark');
 
-  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
-  const themeContainerStyle =
-    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+  const themeTextStyle = isDarkMode ? styles.darkThemeText : styles.lightThemeText;
+  const themeContainerStyle = isDarkMode ? styles.darkContainer : styles.lightContainer;
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
     <View style={[styles.container, themeContainerStyle]}>
       <Text style={[styles.text, themeTextStyle]}>Color scheme: {colorScheme}</Text>
+      <Switch
+        value={isDarkMode}
+        onValueChange={toggleTheme}
+        trackColor={{ false: '#d0d0c0', true: '#242c40' }}
+        thumbColor={'#ffffff'}
+      />
       <StatusBar />
     </View>
   );
