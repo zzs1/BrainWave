d70f391fe52c20608d569a7a3acf2afa840bcd26
TakamiInagaki <takami.l.inagaki@gmail.com>
2023-10-12 10:05:51 -0700

    button styles updata

--- components/MoveButton/index.js
+++ components/MoveButton/index.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, View, Pressable, Text} from 'react-native';
 
 export default function MoveButton ({navigation}){
 
     const moveButtonText = "→";
+    const [isPressed, setIsPressed] = useState(false);
+
+    function handlePress(){
+        setIsPressed(true);
+        isPressed ? styles.moveButtonPressed : styles.moveButton;
+    }
 
     return(
         <View style={styles.container}>
             {/* <StatusBar style="auto" /> */}
                 <View>
-                    <Pressable style={styles.moveButton} onPress= {() => navigation.push('/')}>
+                    <Pressable style={styles.moveButton} onPress= {() => { handlePress(); navigation.push('/');}}>
                         <Text style={styles.moveButtonArrow}>{moveButtonText}</Text>
                     </Pressable>
                 
@@ -36,6 +43,22 @@ const styles =  StyleSheet.create({
         shadowRadius: 3,
         shadowOpacity: 0.9,
         elevation: 4,
+    },
+    moveButtonPressed: {
+        width: 70,
+        height: 60,
+        backgroundColor: '#0C7BDC',
+        borderRadius: 10, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: '#b4b4b4',
+        shadowOffset: {
+            width: 5, 
+            height: 5,
+        },
+        shadowRadius: 3,
+        shadowOpacity: 0.9,
+        elevation: 0,
     },
     moveButtonArrow: {
         fontSize: 30,
