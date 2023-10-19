9b046361cd6bc6264f7ce3df27b599b08931d25d
Navid Memari <work.nmemari@gmail.com>
2023-10-18 17:15:35 -0700

    created skeletons

---
+++ components/Molecules/AccountCard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View, Button} from 'react-native';
+
+export default function AccountCard({
+    
+}) {
+  return (
+    <View>
+        
+    </View>
+  )
+}
---
+++ components/Molecules/SideMenuItem/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View, Button} from 'react-native';
+
+export default function SideMenuItem({
+    
+}) {
+  return (
+    <View>
+        
+    </View>
+  )
+}
