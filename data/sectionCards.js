fab399286840b424eaa33a92d57f5e6abff39544
Navid Memari <work.nmemari@gmail.com>
2023-10-21 17:49:56 -0700

    added data

---
+++ data/sectionCards.js
@@ -0,0 +1,14 @@
+export const sectionCard = [
+    {
+        title: "Numbers Problems",
+        image: require('../assets/Illustrations/Numbers/Numbers1.png'),
+    },
+    {
+        title: "Logic Problems",
+        image: require('../assets/Illustrations/Logic1.png'),
+    },
+    {
+        title: "Pattern Recognition",
+        image: require('../assets/Illustrations/Pattern/Pattern1.png'),
+    }
+]
