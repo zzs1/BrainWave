df927b6f07702e81b930d6657b34bdf9cb4a8c0c
Navid Memari <work.nmemari@gmail.com>
2023-10-21 17:49:49 -0700

    updated components

--- components/Molecules/SectionCardSide/index.js
+++ components/Molecules/SectionCardSide/index.js
@@ -3,31 +3,40 @@ import React from 'react'
 import { StyleSheet, Text, View, Button} from 'react-native';
 import { Image } from 'expo-image';
 
-import ProgressBar from '../../Atoms/ProgressBar-level';
-
 export default function SectionCardSide({
-    color=[],
+    color='',
+    border='',
     theme='',
     image='',
     title='',
 }) {
   return (
     <View style={{
-      border: `3px solid ${color[1]}`, 
-      backgroundColor: theme === 'dark' ? color[0] : `linear-gradient(white, ${color[1]})`,
-      width: 170,
-      height: 245,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center'
+      ...styles.container,
+      border: `3px solid ${border}`, 
+      backgroundColor: theme === 'dark' ? color : `linear-gradient(white, ${color})`,
     }}>
         <Text style={{
           fontSize: 16,
+          fontWeight: 'bold',
+          color: theme === 'dark' ? 'white' : '#1E1E1E'
         }}>{title}</Text>
-        <Image source={image} style={{
+        <Image source={image} contentFit='contain' style={{
           width: 100,
           height: 100
         }}/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 170,
+    height: 245,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    borderRadius: 10
+  }
+})
