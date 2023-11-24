73e5d0c5f0e7973ce33951f8acc71b280ddc89cd
Aaron Tripp <aarontripp83@gmail.com>
2023-11-23 17:38:47 -0800

    add wimmy thinking lottie

---
+++ components/Atoms/WimmyThinking/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Pressable, Text, Dimensions} from 'react-native';
+import LottieView from 'lottie-react-native';
+import { useTheme } from '@react-navigation/native';
+import animationData from '../../../assets/Animations/wimmy-thinking.json';
+
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+
+export default function WimmyThinking(){
+    return (
+    <View style={styles.animationContainer}>
+        <LottieView
+          autoPlay
+          ref={(animationRef) => animationRef && animationRef.play()}
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          source={animationData}
+        />
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    animationContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
