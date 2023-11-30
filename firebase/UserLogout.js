eda7911341a2e87ecf20bcf39ae5b8d721e54c86
Navid Memari <work.nmemari@gmail.com>
2023-11-29 17:07:59 -0800

    added user forms

---
+++ firebase/UserLogout.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { StyleSheet, View, Pressable, Dimensions, Text } from 'react-native';
+import { getAuth, signOut } from 'firebase/auth';
+
+import PrimaryButton from '../components/Atoms/PrimaryButton';
+
+import { AppContext } from '../context/AppContext';
+
+export default function UserLogOut() {
+    const {
+        userName, setUserName,
+        wimPoints, setWimPoints,
+        pfp, setPfp,
+        setNumberProgress, setNumberLevel,
+        setLogicProgress, setLogicLevel,
+        setPatternProgress, setPatternLevel,
+        isDyslexic
+    } = React.useContext(AppContext);
+
+    const logout = async () => {
+        const auth = getAuth();
+        await signOut(auth);
+        setUserName("");
+        setWimPoints(0);
+        setPfp("");
+        setNumberProgress(0);
+        setNumberLevel(0);
+        setLogicProgress(0);
+        setLogicLevel(0);
+        setPatternProgress(0);
+        setPatternLevel(0);
+        console.log("User Logged Out");
+    }
+
+  return (
+    <View>
+        <PrimaryButton name='Log Out' onPress={() => logout()}/>
+    </View>
+  )
+}
