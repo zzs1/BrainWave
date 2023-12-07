5c72586170a710f3d46be9fdafe106cb7288b981
Ivona <ivona.i.work@gmail.com>
2023-12-06 23:17:47 -0800

    fixed speech

--- components/Atoms/DialogueBoxLower/index.js
+++ components/Atoms/DialogueBoxLower/index.js
@@ -10,8 +10,10 @@ import { AppContext } from '../../../context/AppContext.js'
 import WimmyThinking from '../WimmyThinking/index.js';
 import SoundBlack from '../../../assets/Icons/sound-black.svg'
 import SoundWhite from '../../../assets/Icons/sound-white.svg'
+
 import SoundIconPause from '../../../assets/Icons/SoundIconPause.png'
 import VolumeBlack from '../../../assets/Icons/VolumeBlack.png'
+import VolumePause from '../../../assets/Icons/sound-icon-pause.svg'
 
 export default function DialogueBoxLower({
     title = '',
@@ -31,10 +33,19 @@ export default function DialogueBoxLower({
 
     React.useEffect(() => {
         listAvailableVoices();
+        
+        return() => {
+            Speech.stop()
+        }
     }, []);
+    React.useEffect(() => {
+        if(!isPlaying && Speech) {
+            Speech.stop();
+        }
+    }, [isPlaying])
 
     const WimmySpeak = () => {
-        
+       
         console.log("SPEAKING");
         const speaking = `${desc}`;
         options = {
@@ -48,10 +59,10 @@ export default function DialogueBoxLower({
     const playPause = () => {
         const preValue = isPlaying;
         setIsPlaying(!preValue);
-        if (preValue) {
+        if (!preValue) {
             WimmySpeak()
-        } else {
-           speechInstance && Speech.onPause()
+        } else{
+            Speech.stop()
         }
         
     }
@@ -92,20 +103,21 @@ export default function DialogueBoxLower({
                 <Pressable  onPress={playPause}>
                 {
                     isPlaying ?
-                    <Image source={require('../../../assets/Icons/SoundIconPause.png')}style={{
-                        width: 40,
-                        height: 40,
-                    }} contentFit='contain'/>
-                    
-               
-                    : <Image 
+                    <Image 
                     source={dark ? SoundWhite : SoundBlack}
                     contentFit='contain'
                     style={{
                         width: 40,
                         height: 40,
                     }}
-/> }
+/>
+                  
+                    
+               
+                    :   <Image source={require('../../../assets/Icons/sound-icon-pause.svg')}style={{
+                        width: 40,
+                        height: 40,
+                    }} contentFit='contain'/> }
                      
                 
                 </Pressable>
