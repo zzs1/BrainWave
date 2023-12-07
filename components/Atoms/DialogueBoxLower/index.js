13bcaa63cd465a04ae07a1a8f4dfbfed6cf09393
Ivona <ivona.i.work@gmail.com>
2023-12-06 19:21:22 -0800

    adjustments

--- components/Atoms/DialogueBoxLower/index.js
+++ components/Atoms/DialogueBoxLower/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
-import { StyleSheet, Text, View, Button, Pressable, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import * as Speech from 'expo-speech';
 import { Image } from 'expo-image';
@@ -10,6 +10,8 @@ import { AppContext } from '../../../context/AppContext.js'
 import WimmyThinking from '../WimmyThinking/index.js';
 import SoundBlack from '../../../assets/Icons/sound-black.svg'
 import SoundWhite from '../../../assets/Icons/sound-white.svg'
+import SoundIconPause from '../../../assets/Icons/SoundIconPause.png'
+import VolumeBlack from '../../../assets/Icons/VolumeBlack.png'
 
 export default function DialogueBoxLower({
     title = '',
@@ -20,22 +22,40 @@ export default function DialogueBoxLower({
     const { isDyslexic } = React.useContext(AppContext);
     const { colors, dark } = useTheme();
 
+
     const listAvailableVoices = async () => {
         let voice = await Speech.getAvailableVoicesAsync()
         console.log(voice)
     }
+    let speechInstance;
 
     React.useEffect(() => {
         listAvailableVoices();
     }, []);
 
     const WimmySpeak = () => {
+        
+        console.log("SPEAKING");
         const speaking = `${desc}`;
         options = {
-            voice: "com.apple.speech.synthesis.voice.Fred"
+            voice: "en-us-x-iol-local"
         }
-        Speech.speak(speaking)
+     
+       Speech.speak(speaking,options)
+
     }
+    const [isPlaying, setIsPlaying] = useState(false);
+    const playPause = () => {
+        const preValue = isPlaying;
+        setIsPlaying(!preValue);
+        if (preValue) {
+            WimmySpeak()
+        } else {
+           speechInstance && Speech.onPause()
+        }
+        
+    }
+ 
     return (
         <View style={{
             ...styles.dialogue_box,
@@ -48,15 +68,12 @@ export default function DialogueBoxLower({
                 fontFamily: isDyslexic ? 'Lexend-Bold' : 'Poppins-Bold'
             }}>{title}</Text>
             {
-                loading ? <WimmyThinking /> : <ScrollView style={{
-                    maxHeight: 400
-                }}>
+                loading ? <WimmyThinking /> :
                     <Text style={{
                         ...styles.desc,
                         color: colors.text,
                         fontFamily: isDyslexic ? 'Lexend-Regular' : 'Poppins-Regular'
                     }}>{desc}</Text>
-                </ScrollView>
             }
             <View style={{
                 display: 'flex',
@@ -71,15 +88,26 @@ export default function DialogueBoxLower({
                     color: colors.fadedText,
                     fontFamily: isDyslexic ? 'Lexend-Regular' : 'Poppins-Regular'
                 }}>{instuction}</Text>
-                <Pressable onPress={WimmySpeak}>
-                    <Image 
-                        source={dark ? SoundWhite : SoundBlack}
-                        contentFit='contain'
-                        style={{
-                            width: 40,
-                            height: 40,
-                        }}
-                    />
+           
+                <Pressable  onPress={playPause}>
+                {
+                    isPlaying ?
+                    <Image source={require('../../../assets/Icons/SoundIconPause.png')}style={{
+                        width: 40,
+                        height: 40,
+                    }} contentFit='contain'/>
+                    
+               
+                    : <Image 
+                    source={dark ? SoundWhite : SoundBlack}
+                    contentFit='contain'
+                    style={{
+                        width: 40,
+                        height: 40,
+                    }}
+/> }
+                     
+                
                 </Pressable>
             </View>
         </View>
